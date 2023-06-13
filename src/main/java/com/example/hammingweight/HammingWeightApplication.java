bb26ceda9eaff7a67b6c6b7146b6e03c7949007e
yvanorek <yessica.vanorek@gmail.com>
2023-06-13 11:31:42 +0200

    Initial commit

---
+++ src/main/java/com/example/hammingweight/HammingWeightApplication.java
@@ -0,0 +1,13 @@
+package com.example.hammingweight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HammingWeightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HammingWeightApplication.class, args);
+    }
+
+}
